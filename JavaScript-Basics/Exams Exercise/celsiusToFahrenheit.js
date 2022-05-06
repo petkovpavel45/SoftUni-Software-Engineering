be7815634cd57b8b2eca85aab3eab4ef637c58ef
Pavel Petkov <petkovpavel45@gmail.com>
2022-05-06 20:31:08 +0300

    exam exercise

---
+++ JavaScript-Basics/Exams Exercise/celsiusToFahrenheit.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit (input){
+    let celsius = Number(input[0]);
+    let fahrenheit = celsius * 1.8 + 32;
+    console.log(fahrenheit.toFixed(2));
+}
+celsiusToFahrenheit(["25"])
