e94f85499c8a2823244d99c1e1dd080f89c43161
Pavel Petkov <petkovpavel45@gmail.com>
2022-05-06 20:31:45 +0300

    exam exercise

---
+++ JavaScript-Basics/Exams Exercise/changeBureau.js
@@ -0,0 +1,14 @@
+function changeBureau(input){
+   let bitcoin = Number(input[0]);
+   let chineseYuan = Number(input[1]);
+   let commission = Number(input[2]);
+
+   let oneBitcoin = 1168;
+   let sumYuan = chineseYuan * 0.75;
+   let usdToBgn = 0.75 = 1.32;
+   let 
+
+}
+changeBureau(["1", 
+"5", 
+"5"])
