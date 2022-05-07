133ded216662f102184f16b1cda1a11933653f90
Pavel Petkov <petkovpavel45@gmail.com>
2022-05-07 15:00:58 +0300

    exams exercise

---
+++ JavaScript-Basics/Exams Exercise/dishwasher.js
@@ -0,0 +1,13 @@
+function dishwasher(input) {
+    let countBottles = Number(input[0]);
+    let totalDetergent = countBottles * 750;
+
+    let command = input[index];
+    index++;
+
+    while(command !== "End") {
+        
+        
+    }
+}
+dishwasher(["2", "53", "65", "55", "End"])
