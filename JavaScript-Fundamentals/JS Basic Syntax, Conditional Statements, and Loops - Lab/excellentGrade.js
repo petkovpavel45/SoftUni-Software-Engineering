81fd26aeef3d18aee820a8757a630e9637dc3c07
Pavel Petkov <petkovpavel45@gmail.com>
2022-05-21 20:53:02 +0300

    lab exercises

---
+++ JavaScript-Fundamentals/JS Basic Syntax, Conditional Statements, and Loops - Lab/excellentGrade.js
@@ -0,0 +1,7 @@
+function excellentGrade(grade) {
+    console.log(grade >= 5.50 ? "Excellent" : "Not excellent");
+}
+excellentGrade(5.50)
+excellentGrade(4.35)
+excellentGrade(6.50)
+excellentGrade(2.50)
