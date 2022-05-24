5df63273a5864512b1647035d1a46cef3cf5c29a
Pavel Petkov <petkovpavel45@gmail.com>
2022-05-24 19:32:30 +0300

    exercises

---
+++ JavaScript-Fundamentals/JS Basic Syntax, Conditional Statements, and Loops - Exercise/bitcoin.js
@@ -0,0 +1,21 @@
+function bitcoinMining(arr) {
+  let total = 0;
+  let firstbuy = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let currentDay = arr[i] * 67.51;
+
+    if ((i + 1) % 3 === 0) {
+      currentDay = currentDay * 0.7;
+    }
+    total += currentDay;
+    if (total >= 11949.16 && firstbuy == 0) {
+      firstbuy = i + 1;
+    }
+  }
+  console.log(`Bought bitcoins: ${Math.floor(total / 11949.16)}`);
+  if (firstbuy > 0) {
+    console.log(`Day of the first purchased bitcoin: ${firstbuy}`);
+  }
+  console.log(`Left money: ${(total % 11949.16).toFixed(2)} lv.`);
+}
+bitcoinMining([3124.15, 504.212, 2511.124]);
