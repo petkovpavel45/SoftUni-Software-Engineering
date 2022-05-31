7086d2e5fbff44e6e6af9011862eccd1d2f4d81b
Pavel Petkov <petkovpavel45@gmail.com>
2022-05-31 12:11:13 +0300

    exercises

---
+++ JavaScript-Fundamentals/Data Types and Variables - Exercise/07.LowerOrUpper.js
@@ -0,0 +1,8 @@
+function lowerOrUpper(char) {
+    if(char === char.toUpperCase()){
+        console.log("upper-case");
+    }else if(char === char.toLowerCase()){
+        console.log("lower-case");
+    }
+}
+lowerOrUpper("l")
